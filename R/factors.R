40d3a730559b433a7aec5845ba6728c4bb7e7c22
Alex Reinhart <alex@refsmmat.com>
2022-08-05 19:13:45 -0400

    Improve factor function documentation

--- R/factors.R
+++ R/factors.R
@@ -11,6 +11,8 @@
 #'   probabilities. By default, levels are equally likely.
 #' @examples
 #' rfactor(5, c("foo", "bar", "baz"), c(0.4, 0.3, 0.3))
+#' @seealso [by_level()] to assign numeric values based on factor levels, such
+#'   as to set population regression coefficients by factor level
 #' @export
 rfactor <- function(n, levels, prob = 1 / length(levels)) {
   return(factor(sample(levels, size = n, prob = prob, replace = TRUE),
@@ -29,12 +31,26 @@ rfactor <- function(n, levels, prob = 1 / length(levels)) {
 #'   single named vector.
 #' @return Named vector of same length as `x`, with values replaced with those
 #'   specified. Names are the original factor level name.
+#' @seealso [rfactor()] to draw random factor levels, and the `forcats` package
+#'   <https://forcats.tidyverse.org/> for additional factor manipulation tools
 #' @examples
 #' foo <- factor(c("spam", "ham", "spam", "ducks"))
 #'
 #' by_level(foo, spam = 4, ham = 10, ducks = 16.7)
 #'
 #' by_level(foo, c("spam" = 4, "ham" = 10, "ducks" = 16.7))
+#'
+#' # to define a population with a factor that affects the regression intercept
+#' intercepts <- c("foo" = 2, "bar" = 30, "baz" = 7)
+#' pop <- population(
+#'   group = predictor("rfactor",
+#'                     levels = c("foo", "bar", "baz"),
+#'                     prob = c(0.1, 0.6, 0.3)),
+#'   x = predictor("runif", min = 0, max = 10),
+#'   y = response(by_level(group, intercepts) + 0.3 * x,
+#'                error_scale = 1.5)
+#' )
+#' sample_x(pop, 5)
 #' @export
 by_level <- function(x, ...) {
   levels <- list(...)
@@ -45,7 +61,8 @@ by_level <- function(x, ...) {
       # just use that named vector
       levels <- levels[[1]]
     } else {
-      cli_abort("by_level() should be provided either a single named vector or a sequence of named arguments")
+      cli_abort("by_level() should be provided either a single named vector or a sequence of named arguments",
+                class = "regressinator_by_level_arg")
     }
   } else {
     levels <- unlist(levels)
@@ -58,7 +75,8 @@ by_level <- function(x, ...) {
 
     cli_warn(c("not all factor levels were assigned a value in by_level()",
                "*" = "levels without a value: {undef_levels}",
-               "i" = "levels without a value will be given value NA"))
+               "i" = "levels without a value will be given value NA"),
+             class = "regressinator_by_level_missing_level")
   }
 
   return(levels[as.character(x)])
