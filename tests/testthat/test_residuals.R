a88756d11a357737fc201763730bf16ef50febfe
Alex Reinhart <alex@refsmmat.com>
2023-05-01 13:31:58 -0400

    Test partial_residuals() output for GLMs
    I was getting suspicious results, but this shows it's working correctly.

--- tests/testthat/test_residuals.R
+++ tests/testthat/test_residuals.R
@@ -1,3 +1,5 @@
+library(dplyr)
+
 test_that("partial_residuals() produces correct amount of data", {
   fit <- lm(mpg ~ cyl + disp + hp, data = mtcars)
 
@@ -32,6 +34,29 @@ test_that("partial_residuals() rejects factor() in formulas", {
 
   expect_error(partial_residuals(fit),
                class = "regressinator_transmutation_factor")
+})
+
+test_that("partial_residuals() gives correct results for GLMs", {
+  # Can compare to residuals(x, type = "partial") when predictors enter directly
+  # as regressors with no transformations; there may be a constant offset,
+  # however, so center each
+
+  fit <- glm(cyl ~ drat + wt, family = poisson, data = mtcars)
+  pr <- residuals(fit, type = "partial")
+
+  out <- partial_residuals(fit)
+
+  drat_pr <- pr[, "drat"]
+  drat_out <- out |> filter(.predictor_name == "drat") |> pull(.partial_resid)
+
+  expect_equal(unname(drat_pr - mean(drat_pr)),
+               drat_out - mean(drat_out))
+
+  wt_pr <- pr[, "wt"]
+  wt_out <- out |> filter(.predictor_name == "wt") |> pull(.partial_resid)
+
+  expect_equal(unname(wt_pr - mean(wt_pr)),
+               wt_out - mean(wt_out))
 })
 
 test_that("binned_residuals() produces correct amount of data", {
