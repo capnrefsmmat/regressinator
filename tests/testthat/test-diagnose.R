01505882df87e4185dbdbf4a94ddb1525e174e16
Alex Reinhart <alex@refsmmat.com>
2023-09-26 10:30:13 -0400

    Fix a broken test
    There was no y column in data, so the lm() would fail before we could
    test sampling_distribution().

--- tests/testthat/test-diagnose.R
+++ tests/testthat/test-diagnose.R
@@ -7,10 +7,12 @@ test_that("model_lineup rejects invalid fn", {
 
 test_that("sampling_distribution rejects invalid fn", {
   fn <- function(fit) "duck"
-  data <- sample_x(
-    population(x = predictor("rnorm"),
-               y = response(2, error_scale = 1)),
-    n = 10)
+  data <- sample_y(
+    sample_x(
+      population(x = predictor("rnorm"),
+                 y = response(2, error_scale = 1)),
+      n = 10)
+  )
 
   expect_error(sampling_distribution(lm(y ~ x, data = data), data, fn),
                class = "regressinator_diagnostic_class")
